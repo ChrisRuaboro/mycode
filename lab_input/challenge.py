b938dc1af5510844fe43b92ea39e46c1e6daea0c
Chris Ruaboro <christopher.ruaboro@outlook.com>
2021-01-04 18:49:44 +0000

    String challenges

---
+++ lab_input/challenge.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+user_Name = input("Type Your Name:")
+DayOfTheWeek = input("What's the day of the week?")
+print("Hello ", user_Name, "! Today is ", DayOfTheWeek, "!", sep="")
+
