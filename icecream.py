e4f3e65d18eabdcba780fa241ad14a96a232ae74
Chris Ruaboro <christopher.ruaboro@outlook.com>
2021-01-04 22:30:16 +0000

    Complex Lists

---
+++ icecream.py
@@ -0,0 +1,6 @@
+icecream = ["flavors", "salty"]
+icecream.append(int(99))
+print(icecream)
+
+user_Name = input("What is your name?")
+print(f"{icecream[2]} flavors, and {user_Name} chooses to be {icecream[1]}t"
