f3a2895315eb21b4ae49aed81d33a8fe7febed1b
Chris Ruaboro <christopher.ruaboro@outlook.com>
2021-01-07 22:49:08 +0000

    Did some Labs

---
+++ lrnreg/http_parser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import urllib.request
+import re
+
+print("Where should we search?")
+url = input()
+if url.lower() == "quit":
+    quit()
+print("Great! So we'll try to open this url " + str(url) + " to search for the phrase:")
+searchFor = input()
+if searchFor == "quit":
+    quit()
+searchMe = urllib.request.urlopen(url).read().decode("utf-8")
+
+if re.search(searchFor, searchMe):
+    print("Found a match!")
+else:
+    print("No match!")
+
